deff9ba87292df6b75d5a5f578956a8fc6753453
adrienleib <adrien.leib@ynov.com>
2021-03-25 18:14:32 +0100

    delete conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,12 @@
 import { combineReducers } from 'redux'
 import token from './token'
 import user from './user'
-<<<<<<< HEAD
+import stripe from './stripe'
 import nookipedia from './nookipedia'
 
 export default combineReducers({
   token,
   nookipedia,
-  user
-=======
-import stripe from './stripe'
-
-export default combineReducers({
-  token,
-  user,
   stripe
->>>>>>> d15b1ca72b3f15ca236c4bbbb9a936b2cdb5a8ca
+  user
 })
